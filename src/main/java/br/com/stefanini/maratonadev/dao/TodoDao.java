32057a860cadd089386793d4d684f0c261abcea8
melo <rianemlo@gmail.com>
2020-10-22 08:13:04 -0300

    dia 1 maratona

---
+++ src/main/java/br/com/stefanini/maratonadev/dao/TodoDao.java
@@ -0,0 +1,13 @@
+package br.com.stefanini.maratonadev.dao;
+
+import javax.enterprise.context.RequestScoped;
+
+@RequestScoped //a anotação de definição de escopo faz com que se torne um SERVICE
+public class TodoDao {
+
+	public void inserir() {
+		//sql aqui
+	}
+	
+	
+}
